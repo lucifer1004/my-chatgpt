9841b82d060c217f72d78d0c31701075dc0785a8
Gabriel Wu <wuzihua@pku.edu.cn>
2023-03-05 16:50:16 +0800

    fix: export & import

--- components/History.tsx
+++ components/History.tsx
@@ -10,14 +10,75 @@ import { useRouter } from "next/router";
 import { Fragment, useContext, useRef, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { MyChatGPTContext } from "../contexts/MyChatGPTContext";
-import { clearHistory, exportHistory, importHistory } from "../utils";
+import { wrappedWriteClipboard } from "../utils";
 import HistoryItem from "./HistoryItem";
 
 export default function History() {
   const { state, dispatch } = useContext(MyChatGPTContext);
   const [deleteConfirmOpen, setDeleteConfirmOpen] = useState(false);
+  const [importPromptOpen, setImportPromptOpen] = useState(false);
+  const [importedHistory, setImportedHistory] = useState("");
   const router = useRouter();
   const cancelButtonRef = useRef(null);
+  const submitButtonRef = useRef(null);
+
+  async function exportHistory() {
+    try {
+      const indices = JSON.parse(
+        localStorage.getItem("my-chatgpt-indices") || "[]"
+      );
+      const history = [];
+      for (const index of indices) {
+        const messages = localStorage.getItem(index) || "[]";
+        history.push({ index, messages });
+      }
+      await wrappedWriteClipboard(JSON.stringify(history));
+      alert(`导出成功：共${indices.length}条记录`);
+    } catch (e) {
+      alert(`导出失败：${e}`);
+    }
+  }
+
+  async function importHistory(historyStr: string) {
+    try {
+      const history = JSON.parse(historyStr);
+      const indices = JSON.parse(
+        localStorage.getItem("my-chatgpt-indices") || "[]"
+      );
+
+      let added = 0;
+      for (const { index, messages } of history) {
+        if (indices.findIndex((id) => id === index) === -1) {
+          indices.push(index);
+          added++;
+          localStorage.setItem(index, messages);
+        }
+      }
+      localStorage.setItem("my-chatgpt-indices", JSON.stringify(indices));
+      dispatch({ type: "import", indices });
+      alert(
+        `导入成功：共${history.length}条记录，导入${added}条${
+          history.length === added
+            ? ""
+            : `，${history.length - added}条重复记录未导入`
+        }`
+      );
+    } catch (e) {
+      if (historyStr !== null) {
+        alert(`导入失败：${e}`);
+      }
+    }
+  }
+
+  function clearHistory() {
+    const indices = JSON.parse(
+      localStorage.getItem("my-chatgpt-indices") || "[]"
+    );
+    for (const index of indices) {
+      localStorage.removeItem(index);
+    }
+    localStorage.setItem("my-chatgpt-indices", "[]");
+  }
 
   return (
     <div className="mt-5 flex h-screen flex-1 flex-col overflow-hidden">
@@ -49,15 +110,7 @@ export default function History() {
         <button
           className="flex grow items-center justify-center rounded-md p-2 text-indigo-300 hover:bg-indigo-600"
           title="导出全部聊天记录到剪贴板"
-          onClick={async () => {
-            try {
-              await exportHistory();
-              alert("导出成功");
-            } catch (e) {
-              console.error(e);
-              alert("导出失败");
-            }
-          }}
+          onClick={exportHistory}
         >
           <ArrowUpOnSquareIcon
             className="h-6 w-6 flex-shrink-0 text-indigo-700 dark:text-indigo-400"
@@ -67,15 +120,7 @@ export default function History() {
         <button
           className="flex grow items-center justify-center rounded-md p-2 text-indigo-300 hover:bg-indigo-600"
           title="导入从其他地方导出的聊天记录"
-          onClick={async () => {
-            const indices = await importHistory();
-            if (indices) {
-              dispatch({ type: "import", indices });
-              alert("导入成功");
-            } else {
-              alert("导入失败");
-            }
-          }}
+          onClick={() => setImportPromptOpen(true)}
         >
           <ArrowDownOnSquareIcon
             className="h-6 w-6 flex-shrink-0 text-indigo-700 dark:text-indigo-400"
@@ -100,7 +145,7 @@ export default function History() {
           as="div"
           className="relative z-40"
           initialFocus={cancelButtonRef}
-          onClose={setDeleteConfirmOpen}
+          onClose={() => setDeleteConfirmOpen(false)}
         >
           <Transition.Child
             as={Fragment}
@@ -166,6 +211,94 @@ export default function History() {
                       className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                       onClick={() => setDeleteConfirmOpen(false)}
                       ref={cancelButtonRef}
+                    >
+                      取消
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+
+      <Transition.Root show={importPromptOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-40"
+          initialFocus={submitButtonRef}
+          onClose={() => setImportPromptOpen(false)}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-40 overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-indigo-100 sm:mx-0 sm:h-10 sm:w-10">
+                        <ExclamationTriangleIcon
+                          className="h-6 w-6 text-indigo-600"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="mt-3 grow text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <Dialog.Title
+                          as="h3"
+                          className="text-base font-semibold leading-6 text-gray-900"
+                        >
+                          导入聊天记录
+                        </Dialog.Title>
+                        <div className="mt-2">
+                          <textarea
+                            rows={3}
+                            className="focus:shadow-outline w-full appearance-none rounded border bg-white py-2 px-3 text-sm leading-tight text-black shadow focus:outline-none"
+                            value={importedHistory}
+                            onChange={(e) => setImportedHistory(e.target.value)}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button
+                      type="button"
+                      className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 sm:ml-3 sm:w-auto"
+                      onClick={async () => {
+                        await importHistory(importedHistory);
+                        setImportPromptOpen(false);
+                        setImportedHistory("");
+                      }}
+                      ref={submitButtonRef}
+                    >
+                      导入
+                    </button>
+                    <button
+                      type="button"
+                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                      onClick={() => {
+                        setImportPromptOpen(false);
+                        setImportedHistory("");
+                      }}
                     >
                       取消
                     </button>
--- utils/index.ts
+++ utils/index.ts
@@ -1,28 +1,9 @@
-export async function wrappedReadClipboard() {
-  if (typeof navigator.clipboard === "undefined") {
-    const textArea = document.createElement("textarea");
-    textArea.value = "";
-    textArea.style.position = "fixed";
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
-
-    let historyStr: string;
-    if (document.execCommand("paste")) {
-      historyStr = textArea.value;
-    } else {
-      // Use prompt as a fallback
-      historyStr = window.prompt("请将需要导入的历史记录粘贴在这里：");
-    }
-    document.body.removeChild(textArea);
-    return historyStr;
-  } else {
-    return await navigator.clipboard.readText();
-  }
-}
-
 export async function wrappedWriteClipboard(str: string) {
-  if (typeof navigator.clipboard === "undefined") {
+  try {
+    await navigator.clipboard.writeText(str);
+  } catch (e) {
+    console.warn(e);
+
     const textArea = document.createElement("textarea");
     textArea.value = str;
     textArea.style.position = "fixed";
@@ -30,13 +11,13 @@ export async function wrappedWriteClipboard(str: string) {
     textArea.focus();
     textArea.select();
 
+    // Use deprecated `execCommand` as fallback
     const success = document.execCommand("copy");
     if (!success) {
+      document.body.removeChild(textArea);
       throw new Error("Failed to write to clipboard.");
     }
     document.body.removeChild(textArea);
-  } else {
-    return await navigator.clipboard.writeText(str);
   }
 }
 
@@ -55,52 +36,4 @@ export function getSummary(uid) {
       JSON.parse(localStorage.getItem(uid))[0].content.substr(0, 20) + "..."
     );
   }
-}
-
-interface HistoryRecord {
-  index: string;
-  messages: string;
-}
-
-export async function exportHistory() {
-  const indices = JSON.parse(
-    localStorage.getItem("my-chatgpt-indices") || "[]"
-  );
-  const history = [];
-  for (const index of indices) {
-    const messages = localStorage.getItem(index) || "[]";
-    history.push({ index, messages });
-  }
-  await wrappedWriteClipboard(JSON.stringify(history));
-}
-
-export async function importHistory() {
-  try {
-    const historyStr = await wrappedReadClipboard();
-    const history: HistoryRecord[] = JSON.parse(historyStr);
-    const indices = JSON.parse(
-      localStorage.getItem("my-chatgpt-indices") || "[]"
-    );
-    for (const { index, messages } of history) {
-      if (indices.findIndex((id) => id === index) === -1) {
-        indices.push(index);
-        localStorage.setItem(index, messages);
-      }
-    }
-    localStorage.setItem("my-chatgpt-indices", JSON.stringify(indices));
-    return indices;
-  } catch (e) {
-    console.error(e);
-    return null;
-  }
-}
-
-export function clearHistory() {
-  const indices = JSON.parse(
-    localStorage.getItem("my-chatgpt-indices") || "[]"
-  );
-  for (const index of indices) {
-    localStorage.removeItem(index);
-  }
-  localStorage.setItem("my-chatgpt-indices", "[]");
 }
